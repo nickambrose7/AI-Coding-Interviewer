3660e4683f6e0d02bcd1cad60870f34392cd339b
nickambrose7 <nickambrose7@gmail.com>
2024-04-10 12:03:30 -0700

    Split working

--- my-app/src/components/Workspace/ProblemDescription/ProblemDescription.tsx
+++ my-app/src/components/Workspace/ProblemDescription/ProblemDescription.tsx
@@ -1,11 +1,57 @@
 import React from 'react';
 
+import { auth, firestore } from "@/firebase/firebase";
+import { arrayRemove, arrayUnion, doc, getDoc, runTransaction, updateDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { AiFillLike, AiFillDislike, AiOutlineLoading3Quarters, AiFillStar } from "react-icons/ai";
+import { BsCheck2Circle } from "react-icons/bs";
+import { TiStarOutline } from "react-icons/ti";
 type ProblemDescriptionProps = {
     
 };
 
 const ProblemDescription:React.FC<ProblemDescriptionProps> = () => {
     
-    return <div>Probelm description</div>
+    return (
+		<div className='bg-dark-layer-1'>
+			{/* TAB */}
+			<div className='flex h-11 w-full items-center pt-2 bg-dark-layer-2 text-white overflow-x-hidden'>
+				<div className={"bg-dark-layer-1 rounded-t-[5px] px-5 py-[10px] text-xs cursor-pointer"}>
+					Description
+				</div>
+			</div>
+
+			<div className='flex px-0 py-4 h-[calc(100vh-94px)] overflow-y-auto'>
+				<div className='px-5'>
+					{/* Problem heading */}
+					<div className='w-full'>
+						<div className='flex space-x-4'>
+							<div className='flex-1 mr-2 text-lg text-white font-medium'>1. Two Sum</div>
+						</div>
+
+						{/* Problem Statement(paragraphs) */}
+						<div className='text-white text-sm'>
+							This is a problem.
+						</div>
+
+						{/* Examples */}
+						<div className='mt-4 text-white'>
+							Here are the examples
+						</div>
+
+						{/* Constraints */}
+						<div className='my-8 pb-4'>
+							<div className='text-white text-sm font-medium'>Constraints:</div>
+							<ul className='text-white ml-5 list-disc '>
+								Constraint 1
+                                constraint 2
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 export default ProblemDescription;
